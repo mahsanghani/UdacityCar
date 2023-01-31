3ef564160b66fb5efe0171a30966ed4aaaf05aa9
mahsanghani <ghaniahsan@outlook.com>
2023-01-31 16:28:07 -0500

    load train dev test

--- 3/Traffic_Sign_Classifier.py
+++ 3/Traffic_Sign_Classifier.py
@@ -8,3 +8,31 @@ import cv2
 from IPython import get_ipython
 
 get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+training_file = "../data/train.p"
+validation_file= "../data/valid.p"
+testing_file = "../data/test.p"
+
+with open(training_file, mode='rb') as f:
+    train = pickle.load(f)
+with open(validation_file, mode='rb') as f:
+    valid = pickle.load(f)
+with open(testing_file, mode='rb') as f:
+    test = pickle.load(f)
+
+X_train, y_train = train['features'], train['labels']
+X_valid, y_valid = valid['features'], valid['labels']
+X_test, y_test = test['features'], test['labels']
+
+assert(len(X_train) == len(y_train))
+assert(len(X_valid) == len(y_valid))
+assert(len(X_test) == len(y_test))
+
+print("X_train shape:", X_train[0].shape)
+print("y_train shape:", y_train.shape)
+print("X_valid shape:", X_valid[0].shape)
+print("y_valid shape:", y_valid.shape)
+print("X_test shape:", X_test[0].shape)
+print("y_test shape:", y_test.shape)
+
